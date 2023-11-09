f4a3dd54f6b0b8fdda42ec867b61c19e9b335034
Gregorius Avip <gregorius.avip@gmail.com>
2023-11-09 00:08:29 -0800

    Update Entity.java

--- src/main/java/greg/minitwitter/user/entity/Entity.java
+++ src/main/java/greg/minitwitter/user/entity/Entity.java
@@ -3,7 +3,7 @@ package greg.minitwitter.user.entity;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public interface Entity {
-    public void Display(DefaultMutableTreeNode node);
-    public String getID();
-    public String toString();
+    void Display(DefaultMutableTreeNode node);
+    String getID();
+    String toString();
 }
