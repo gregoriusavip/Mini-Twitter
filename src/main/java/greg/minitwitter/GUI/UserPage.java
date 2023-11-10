2b3fe91f030653f7c079a84af37f1e1bbdd27ed8
Gregorius Avip <gregorius.avip@gmail.com>
2023-11-09 22:15:05 -0800

    Design User Page UI

--- src/main/java/greg/minitwitter/GUI/UserPage.java
+++ src/main/java/greg/minitwitter/GUI/UserPage.java
@@ -1,4 +1,15 @@
 package greg.minitwitter.GUI;
 
+import javax.swing.*;
+
 public class UserPage {
+    private JPanel panel1;
+    private JButton followButton;
+    private JTextField followUserTextField;
+    private JList list1;
+    private JTextArea textArea1;
+    private JButton tweetButton;
+    private JLabel FollowingLabel;
+    private JList list2;
+    private JLabel UserLabel;
 }
