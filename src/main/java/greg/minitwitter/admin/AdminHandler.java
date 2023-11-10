02504afab92e80834987f21901aa4db827ff031e
Gregorius Avip <gregorius.avip@gmail.com>
2023-11-09 16:21:22 -0800

    Update AdminHandler.java
    Make AdminHandler public and serves as the class manager for User and Group
    
    Add getTotalUser method to handle getting toatal user

--- src/main/java/greg/minitwitter/admin/AdminHandler.java
+++ src/main/java/greg/minitwitter/admin/AdminHandler.java
@@ -1,12 +1,15 @@
 package greg.minitwitter.admin;
 
+import greg.minitwitter.user.entity.Entity;
 import greg.minitwitter.user.entity.Group;
 import greg.minitwitter.user.entity.User;
+import greg.minitwitter.user.entity.visitor.CountUserVisitor;
 
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.util.HashSet;
 import java.util.Set;
 
-class AdminHandler {
+public class AdminHandler {
 
     private static volatile AdminHandler instance;
     private final Set<String> entities = new HashSet<>();
@@ -24,21 +27,30 @@ class AdminHandler {
         }
         return instance;
     }
-    public boolean addUser(User user){
+    public boolean addUser(String userID, Group group){
+        User user = new User(userID, group);
         if(!entities.contains(user.getID())){
-            System.out.println("User not found");   //debug
             entities.add(user.getID());
             return user.getGroup().addUser(user);
         }
         return false;
     }
-
-    public boolean addGroup(Group group) {
-        if (!entities.contains(group.getID())) {
+    public Integer getTotalUser(Group root){
+        return root.accept(new CountUserVisitor());
+    }
+    public boolean addGroup(String groupID, Group group){
+        Group newGroup = new Group(groupID, group);
+        if (!entities.contains(newGroup.getID())) {
             System.out.println("Group not found");   //debug
-            entities.add(group.getID());
-            return group.getGroup().addGroup(group);
+            entities.add(newGroup.getID());
+            return newGroup.getGroup().addGroup(newGroup);
         }
         return false;
+    }
+
+    public void Display(Group root, DefaultMutableTreeNode node){
+        for(Entity entity : root.getSet()){
+            entity.Display(node);
+        }
     }
 }
