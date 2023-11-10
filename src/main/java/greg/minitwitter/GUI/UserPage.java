6da71cef96622b6c7910ac7cf35e8bdf031462ac
Gregorius Avip <gregorius.avip@gmail.com>
2023-11-09 22:22:24 -0800

    Update UI

--- src/main/java/greg/minitwitter/GUI/UserPage.java
+++ src/main/java/greg/minitwitter/GUI/UserPage.java
@@ -6,10 +6,10 @@ public class UserPage {
     private JPanel panel1;
     private JButton followButton;
     private JTextField followUserTextField;
-    private JList list1;
     private JTextArea textArea1;
     private JButton tweetButton;
     private JLabel FollowingLabel;
     private JList list2;
     private JLabel UserLabel;
+    private JList list1;
 }
