d34b65ecdec05b3001785413f1013e7a32f9bd3f
Gregorius Avip <gregorius.avip@gmail.com>
2023-11-13 03:11:18 -0800

    Add the new method to AdminHandler

--- src/main/java/greg/minitwitter/admin/AdminHandler.java
+++ src/main/java/greg/minitwitter/admin/AdminHandler.java
@@ -4,6 +4,8 @@ import greg.minitwitter.entity.Entity;
 import greg.minitwitter.entity.Group;
 import greg.minitwitter.entity.Root;
 import greg.minitwitter.entity.visitor.CountGroupVisitor;
+import greg.minitwitter.entity.visitor.CountPositiveMessage;
+import greg.minitwitter.entity.visitor.CountTotalMessages;
 import greg.minitwitter.entity.visitor.CountUserVisitor;
 
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -36,6 +38,18 @@ public class AdminHandler {
     }
     public Integer getTotalGroup(){
         return root.accept(new CountGroupVisitor());
+    }
+    public Integer getTotalMessages() { return root.accept(new CountTotalMessages()); }
+    private Integer getTotalPositiveMessages() { return root.accept(new CountPositiveMessage()); }
+    public Double getPositiveMessagePercentage() {
+        int denominator = getTotalMessages();
+        if (denominator == 0){
+            return 0.0;
+        }
+        int numerator = getTotalPositiveMessages();
+        double result = (double) numerator / denominator;
+        result *= 100;
+        return result;
     }
     public void Display(DefaultMutableTreeNode node){
         for(Entity entity : root.getSet()){
